8df6f753bbc103b9789d688eb875ae99580d0529
Slavikus1 <vyacheslav921@icloud.com>
2026-03-24 23:36:44 +0300

    JobIntentService

---
+++ app/src/main/java/com/example/services/MyJobIntentService.kt
@@ -0,0 +1,54 @@
+package com.example.services
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.app.JobIntentService
+
+class MyJobIntentService : JobIntentService() {
+
+    @Deprecated("Deprecated in Java")
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onHandleWork(intent: Intent) {
+        val page = intent.getIntExtra(PAGE, 0)
+        log("onHandleWork")
+        for (i in 1..5) {
+            Thread.sleep(1000)
+            log("Timer $i page: $page")
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onDestroy() {
+        log("onDestroy")
+        super.onDestroy()
+    }
+
+    private fun log(message: String) {
+        Log.i("MyJobIntentService", message)
+    }
+
+
+    companion object {
+        private const val PAGE = "page"
+        private const val JOB_ID = 21
+
+        fun enqueue(context: Context, page: Int) {
+            enqueueWork(
+                context, MyJobIntentService::class.java,
+                JOB_ID, newIntent(context, page)
+            )
+        }
+
+        private fun newIntent(context: Context, page: Int): Intent {
+            return Intent(context, MyJobIntentService::class.java).apply {
+                putExtra(PAGE, page)
+            }
+        }
+    }
+}
+
